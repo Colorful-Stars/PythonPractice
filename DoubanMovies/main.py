2a5c9ae0e43280c1ef8c57485e10f8d2666a1527
Colorful-Stars <songpo.zhang@foxmail.com>
2017-12-10 14:30:40 +0800

    add some

--- DoubanMovies/main.py
+++ DoubanMovies/main.py
@@ -13,12 +13,12 @@ import requests
 from bs4 import BeautifulSoup
 import pymysql
 
+
+
 ''''' 
 User-Agent 
 构建浏览器代理 
 '''
-
-
 def getUA():
     UA_list = [
         "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
@@ -36,8 +36,6 @@ def getUA():
 Cookie池 
 
 '''
-
-
 def getCookie():
     cookie_list = [
         'll="118237"; bid=KvZaCPoQKJk; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1490332252%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _vwo_uuid_v2=912FADCFB9317239E37ED315ED9F488F|7ad5255e106b04510fc5d59ed2abc07b; _pk_id.100001.4cf6=490a4f915c676455.1490320279.2.1490333053.1490320313.; _pk_ses.100001.4cf6=*; __utma=30149280.90895996.1490320277.1490320277.1490332251.2; __utmb=30149280.1.10.1490332251; __utmc=30149280; __utmz=30149280.1490332251.2.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utma=223695111.2019742000.1490320279.1490320279.1490332252.2; __utmb=223695111.0.10.1490332252; __utmc=223695111; __utmz=223695111.1490332252.2.2.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/',
@@ -59,8 +57,6 @@ def getCookie():
 ''''' 
 构建代理IP池 
 '''
-
-
 def proxypool(num):
     n = 1
     # os.chdir(r'/Users/apple888/PycharmProjects/proxy IP')
@@ -83,15 +79,12 @@ getMoviePakge()
 此函数用于获取电影列表页面的url链接 
 返回值为列表pakge_urls 
 '''
-
-
 def getMoviePakge():
     pakge_urls = []
-    #for i in range(0, 7821, 20):
-        #url = 'https://movie.douban.com/tag/%E7%88%B1%E6%83%85?start=' + str(i) + '&type=T'
-    url = 'https://movie.douban.com/tag/%E7%88%B1%E6%83%85?start=0&type=T'
+    for i in range(0, 7781, 20):
+        url = 'https://movie.douban.com/tag/%E7%88%B1%E6%83%85?start=' + str(i) + '&type=T'
         # print(url)
-    pakge_urls.append(url)
+        pakge_urls.append(url)
     return pakge_urls
 
 
@@ -100,8 +93,6 @@ getMovieUrls()
 此函数用于获取电影的链接用于后面的信息的访问url入口 
 返回一个列表，里面存储着电影链接 
 '''
-
-
 def getMovieUrls(pakgeurl, cookie, userAgent, proxys):
     movie_urls = []
     time.sleep(random.uniform(1.6, 2.1))
@@ -127,10 +118,9 @@ def getMovieUrls(pakgeurl, cookie, userAgent, proxys):
     return movie_urls
 
 
-''''' 
-
 '''
 
+'''
 
 def getMovieData(movieurl, cookie, userAgent, proxys):
     global x
@@ -143,7 +133,6 @@ def getMovieData(movieurl, cookie, userAgent, proxys):
                'Host': 'movie.douban.com',
                'Referer': 'https://movie.douban.com/tag/',
                'User-Agent': random.choice(userAgent)}
-    cur = connect.cursor()
 
     moviename = '无'
     director = '无'
@@ -223,7 +212,7 @@ def getMovieData(movieurl, cookie, userAgent, proxys):
 
         # 上映日期
 
-        soup_showtime = obj_bs.find("span", {"property": "v:initialReleaseDate"})
+        soup_showtime = list(obj_bs.find("span", {"property": "v:initialReleaseDate"}))
         showtime = soup_showtime.get_text()
         print('上映日期：' + showtime)
 
@@ -237,15 +226,17 @@ def getMovieData(movieurl, cookie, userAgent, proxys):
 
          # 电影的评价人数
         soup_people = obj_bs.find("span", {"property": "v:votes"})
-        people = int(soup_people.get_text())
-        print('评论人数：', people)
+        if soup_people != None:
+            people = int(soup_people.get_text())
+            print('评论人数：', people)
 
         # 豆瓣评分
         # float类型的grade表示分数
 
         soup_grade = obj_bs.find("strong", {"class": "ll rating_num"})
-        grade = float(soup_grade.get_text())
-        print('评分：', grade)
+        if soup_grade != None:
+            grade = float(soup_grade.get_text())
+            print('评分：', grade)
 
         thismovieurl = movieurl
         print(thismovieurl)
@@ -257,11 +248,11 @@ def getMovieData(movieurl, cookie, userAgent, proxys):
             )
         connect.commit()
     except:
-        print('+++++++++++++++')
+        print('************此条爬取有问题************')
     x = x + 1
 
 
-''''' 
+'''
 运行函数 
 '''
 start = time.clock()
@@ -271,7 +262,7 @@ cur = connect.cursor()
 for pakgeurl in getMoviePakge():
     for movieurl in getMovieUrls(pakgeurl, getCookie(), getUA(), proxypool(150)):
         getMovieData(movieurl, getCookie(), getUA(), proxypool(150))
-        print('《=========================帅气的分隔符================================》')
+        print('《=========================*********==============================》')
 cur.close()
 connect.close()
 end = time.clock()
