873b3ec182717f900cb2fe30c30160a80c6e4119
Colorful-Stars <songpo.zhang@foxmail.com>
2017-12-16 09:54:33 +0800

    dropout overfitting

--- untitled/tensordemo.py
+++ untitled/tensordemo.py
@@ -9,6 +9,11 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+
+
 
 # x_data = np.random.rand(100).astype(np.float32)  # tensorflow 中大部分数据类型是float32
 # y_data = x_data*0.1 + 0.3
@@ -35,7 +40,7 @@ import matplotlib.pyplot as plt
 
 
 
-################ Variable
+####################### Variable
 # state = tf.Variable(0,name='counter')
 # # print(state.name) # print结果是 counter:0 这个0代表的是这是第一个变量
 # one = tf.constant(1) # 常量
@@ -51,7 +56,7 @@ import matplotlib.pyplot as plt
 #         sess.run(updata)
 #         print(sess.run(state))  # 不能是print(state)
 
-#################### Placeholder
+######################### Placeholder
 # input1 = tf.placeholder(tf.float32)
 # input2 = tf.placeholder(tf.float32)
 #
@@ -111,7 +116,68 @@ import matplotlib.pyplot as plt
 #         plt.pause(0.1)
 
 
-#################### Tensorboard可视化
+# ######################## Tensorboard可视化
+# def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
+#     layer_name = 'layer%s'%n_layer
+#     with tf.name_scope(layer_name):
+#         with tf.name_scope('weights'):
+#             Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+#             tf.summary.histogram(layer_name+'/weights',Weights)
+#         with tf.name_scope('biases'):
+#             biases = tf.Variable(tf.zeros([1, out_size]) + 0.1,name='b')
+#             tf.summary.histogram(layer_name+'/biases',biases)
+#         with tf.name_scope('Wx_plus_b'):
+#             Wx_plus_b = tf.matmul(inputs, Weights) + biases
+#         if activation_function is None:
+#             outputs = Wx_plus_b
+#         else:
+#             outputs = activation_function(Wx_plus_b)
+#         tf.summary.histogram(layer_name+'/outputs',outputs)
+#         return outputs
+
+# x_data = np.linspace(-1,1,300)[:,np.newaxis]
+# noise = np.random.normal(0, 0.05, x_data.shape)
+# y_data = np.square(x_data) - 0.5 + noise
+
+# # define placeholder for inputs to network
+# with tf.name_scope('inputs'):
+#     xs = tf.placeholder(dtype = tf.float32,shape = [None,1], name='x_input')
+#     ys = tf.placeholder(dtype = tf.float32,shape = [None,1], name='y_input')
+
+# # hidden layer
+# l1 = add_layer(xs, 1, 10, n_layer=1, activation_function=tf.nn.relu)
+# # output layer
+# prediction = add_layer(l1, 10, 1, n_layer=2, activation_function=None)
+
+# with tf.name_scope('loss'):
+#     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+#     tf.summary.scalar('loss',loss)
+
+# with tf.name_scope('train'):
+#     train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+
+# init = tf.global_variables_initializer()
+# sess = tf.Session()
+# merged = tf.summary.merge_all()
+# writer = tf.summary.FileWriter("logs/",sess.graph)
+# sess.run(init)
+
+# for i in range(1000):
+#     sess.run(train_step, feed_dict={xs:x_data, ys:y_data})
+#     if i % 50 == 0:
+#         re = sess.run(merged, feed_dict={xs:x_data, ys:y_data})
+#         writer.add_summary(re, i)
+
+
+########################## OverFitting with sklearn digits
+# load data
+digits = load_digits()
+X = digits.data
+y = digits.target
+y = LabelBinarizer().fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+
 def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
     layer_name = 'layer%s'%n_layer
     with tf.name_scope(layer_name):
@@ -123,6 +189,7 @@ def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
             tf.summary.histogram(layer_name+'/biases',biases)
         with tf.name_scope('Wx_plus_b'):
             Wx_plus_b = tf.matmul(inputs, Weights) + biases
+            Wx_plus_b = tf.nn.dropout(Wx_plus_b, keep_prob)
         if activation_function is None:
             outputs = Wx_plus_b
         else:
@@ -130,36 +197,32 @@ def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
         tf.summary.histogram(layer_name+'/outputs',outputs)
         return outputs
 
-x_data = np.linspace(-1,1,300)[:,np.newaxis]
-noise = np.random.normal(0, 0.05, x_data.shape)
-y_data = np.square(x_data) - 0.5 + noise
-
-# define placeholder for inputs to network
-with tf.name_scope('inputs'):
-    xs = tf.placeholder(dtype = tf.float32,shape = [None,1], name='x_input')
-    ys = tf.placeholder(dtype = tf.float32,shape = [None,1], name='y_input')
+# define placeholder for inputs ro network
+keep_prob = tf.placeholder(tf.float32)
+xs = tf.placeholder(tf.float32, [None, 64])  # 8*8
+ys = tf.placeholder(tf.float32, [None, 10])  # outputs 0 - 9
 
 # hidden layer
-l1 = add_layer(xs, 1, 10, n_layer=1, activation_function=tf.nn.relu)
+l1 = add_layer(xs, 64, 50, n_layer=1, activation_function=tf.nn.tanh)
 # output layer
-prediction = add_layer(l1, 10, 1, n_layer=2, activation_function=None)
+prediction = add_layer(l1, 50, 10, n_layer=2, activation_function=tf.nn.softmax)
 
-with tf.name_scope('loss'):
-    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
-    tf.summary.scalar('loss',loss)
+# the loss between prediction and real data
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction), reduction_indices=[1]))
+tf.summary.scalar('loss', cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-with tf.name_scope('train'):
-    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
-
-
-init = tf.global_variables_initializer()
 sess = tf.Session()
 merged = tf.summary.merge_all()
-writer = tf.summary.FileWriter("logs/",sess.graph)
+train_writer = tf.summary.FileWriter('logs/train', sess.graph)
+test_writer = tf.summary.FileWriter('logs/test', sess.graph)
+init = tf.global_variables_initializer()
 sess.run(init)
 
-for i in range(1000):
-    sess.run(train_step, feed_dict={xs:x_data, ys:y_data})
+for i in range(500):
+    sess.run(train_step, feed_dict={xs:X_train, ys:y_train, keep_prob:0.5})
     if i % 50 == 0:
-        re = sess.run(merged, feed_dict={xs:x_data, ys:y_data})
-        writer.add_summary(re, i)
+        train_result = sess.run(merged, feed_dict={xs:X_train, ys:y_train, keep_prob:1})
+        test_result = sess.run(merged, feed_dict={xs:X_test, ys:y_test, keep_prob:1})
+        train_writer.add_summary(train_result, i)
+        test_writer.add_summary(test_result, i)
