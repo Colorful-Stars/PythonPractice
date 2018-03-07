8125f0542996acbf88a22c1c4e19aafa6c8c3e54
zspo <songpo.zhang@foxmail.com>
2018-03-07 20:20:20 +0800

    iir filter

---
+++ Matlab_Function/iirFilter/demotest.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding = utf-8 -*-
+
+"""
+ @ Create Time: 2018/3/6
+ @ Author: songpo.zhang
+ @ Target:
+"""
+# import numpy as np
+# a = np.ones(5)
+# print(type(a))
+# b = list(a)
+# print(type(b))
+# print(b)
+# numerator = [
+#                  0.773838953566480,
+#                  - 3.86919476783240,
+#                  7.73838953566480,
+#                  - 7.73838953566480,
+#                  3.86919476783240,
+#                  - 0.773838953566480
+#                  ]
+# print(type(numerator))
+
+# a = [1,2,3,4,5]
+# print(sum(a))
+# numerator = [
+#                  0.773838953566480,
+#                  - 3.86919476783240,
+#                  7.73838953566480,
+#                  - 7.73838953566480,
+#                  3.86919476783240,
+#                  - 0.773838953566480
+#                  ]
+
+# iir_cx = np.ones(6)
+# print(iir_cx[2])
+# a = numerator * iir_cx
+# print(a)
+
+# print(numerator[2:])
+
+for i in range(4,-1,-1):
+    print(i)
+
+# import numpy as np
+# print(np.ones(6))
