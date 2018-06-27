3fbdb1267f9b753ff361c0f0464b3ea76a86176f
zspo <songpo.zhang@foxmail.com>
2018-06-27 18:49:43 +0800

    re_word

--- untitled/wod.py
+++ untitled/wod.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import jieba
  
-text_from_file_with_apath = open('大学门卫老董未删全本(www.lashu8.com).txt').read()
+text_from_file_with_apath = open('recom.txt',encoding='utf-8').read()
 
 wordlist_after_jieba = jieba.cut(text_from_file_with_apath, cut_all = True)
 wl_space_split = " ".join(wordlist_after_jieba)
-print(wl_space_split)
-my_wordcloud = WordCloud(background_color='white',scale=1.5).generate(wl_space_split)
+# print(wl_space_split)
+my_wordcloud = WordCloud(background_color='white',width=1000,height=600,min_font_size=20,max_words=50,max_font_size=80).generate(wl_space_split)
  
 plt.imshow(my_wordcloud)
 plt.axis("off")
