b5879dbcb65cc8d88cae4548ed8ac4762b8acaa8
zspo <songpo.zhang@foxmail.com>
2018-06-28 23:59:46 +0800

    chunk data

--- Some_trick/BigDataChunk.py
+++ Some_trick/BigDataChunk.py
@@ -7,6 +7,8 @@
 
 import pandas as pd
 
+
+# 
 def get_chunck_data(path, chunkSize):
     loop = True
     # chunkSize = 100000
@@ -23,3 +25,27 @@ def get_chunck_data(path, chunkSize):
     df = pd.concat(chunks, ignore_index=True)
     print("chunk data done !")
     return df
+
+######################################################
+# 解决chunk切割带来的风险
+# 针对DF_pingan赛
+
+for data in pd.read_csv(path_data, chunksize=chunksize):
+    # 取出块中最有一个TerminalNo
+    last_terminal_no = data.tail(1)['TERMINALNO'].values[0]
+    # 取出块中除了最后一个TermianlNo的其他数据
+    data_chuck = data[data.TERMINALNO != last_terminal_no].copy()
+
+    # 拼接数据上次遗留的数据
+    if i > 0:
+        data_chuck = pd.concat([re_pd, data_chuck])
+    # 保存最后一个TerminalNo，下次使用
+    re_pd = data[data.TERMINALNO == last_terminal_no].copy()
+
+    # 数据处理
+    preprocess_data_chunk(data_chuck)
+    i += 1
+
+# 最后一个TerminalNo
+preprocess_data_chunk(re_pd)
+######################################################
